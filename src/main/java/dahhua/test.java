f72f4c7b316a912aa13d14c23ef5edb247c81d62
lifeng <www@163.com>
2021-04-15 19:59:52 +0800

    我是韩币

---
+++ src/main/java/dahhua/test.java
@@ -0,0 +1,10 @@
+package dahhua;
+
+public class test {
+    public static void main(String[] args) {
+        int i = 0;
+         int x = 2;
+         int y = i+x;
+          System.out.println(y);
+    }
+}
